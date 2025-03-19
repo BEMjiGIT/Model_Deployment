057cf612d7e3f48c42c196a3417a8cce97a4ff43
Benjamin Eleazar Manafe <150323489+BEMjiGIT@users.noreply.github.com>
2025-03-19 16:23:38 +0700

    Add files via upload

---
+++ Iris_RF_Streamlit.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load("Iris_RF_Model.pkl")
+
+def main():
+    st.title("Machine Leaning Iris Prediction Model Deployment")
+    
+    sepal_length = st.slider('sepal_length', min_value=0.0, max_value=10.0, value=0.1)
+    sepal_width = st.slider('sepal_width', min_value=0.0, max_value=10.0, value=0.1)
+    petal_length = st.slider('petal_length', min_value=0.0, max_value=10.0, value=0.1)
+    petal_width = st.slider('petal_width', min_value=0.0, max_value=10.0, value=0.1)
+    
+    if st.button("Predict"):
+        features = [sepal_length, sepal_width, petal_length, petal_width]
+        result = make_predictions(features)
+        st.success(f"The prediction flower is: {result}")
+        
+    def make_predictions():
+        input_array = np.array(features).reshape(1, -1)
+        prediction = model.predict(input_array)
+        return prediction[0]
+    
+if __name__ == "__main__":
+    main()
