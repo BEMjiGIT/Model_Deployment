af2f5d4d962dbde0466ca29fc799ef960f0de239
Benjamin Eleazar Manafe <150323489+BEMjiGIT@users.noreply.github.com>
2025-03-24 04:26:51 +0700

    Update obesity-streamlit-app.py

--- obesity-prediction-streamlit/obesity-streamlit-app.py
+++ obesity-prediction-streamlit/obesity-streamlit-app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle as pkl
-import sklearn.preprocessing
 
 with open('obesity-prediction-streamlit/rf_model.pkl', 'rb') as file:
     loaded_model = pkl.load(file)
